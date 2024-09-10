a092af85167437a85824ba0cb01f5fc9510c83d3
Klivchenko-Artem <solo30393@gmail.com>
2024-09-10 14:22:13 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <PostsList :posts="posts" />
+  </div>
+</template>
+
+<script>
+import axiosInstance from './components/axios/axios'; 
+import PostsList from './components/UI/PostList.vue'; 
+
+export default {
+  components: {
+    PostsList 
+  },
+  data() {
+    return {
+      posts: [] 
+    };
+  },
+  mounted() {
+    axiosInstance
+      .get('/posts')
+      .then((response) => {
+        this.posts = response.data.data;
+      })
+      .catch((error) => {
+        console.error('Ошибка при получении данных:', error);
+      });
+  }
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+</style>
---
+++ src/components/UI/PostList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>Список постов</h2>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
+        {{ post.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  padding: 10px;
+  background-color: #bababa;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+</style>
